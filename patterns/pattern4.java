0b7769b9129af8f30358015721028459bcb00c87
Manasi Bhangale <manasi.bhangale@siliconmount.com>
2026-03-21 13:27:08 +0530

    message

---
+++ patterns/pattern4.java
@@ -0,0 +1,22 @@
+package patterns;
+import java.util.*;
+class pattern4{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the number of rows:");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+ */
