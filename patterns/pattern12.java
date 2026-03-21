8a0bce56b04c089a7a6bc17bb05cc78be1b87ee8
manasibhangale <manasibhangale2004@gmail.com>
2026-03-21 14:07:30 +0530

    new patterns

--- patterns/pattern12.java
+++ patterns/pattern12.java
@@ -1,32 +1,30 @@
 package patterns;
 import java.util.*;
-class question11 {
+class pattern12 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("enter the number of rows:");
         int n=sc.nextInt();
-       
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=n-i-1;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n - i - 1;j++){
                 System.out.print(" ");
             }
-            System.out.print("*");
             for(int j=1;j<=i;j++){
-                System.out.print("A*");
-                
+                System.out.print(i+j-1);
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(i+j-1);
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
 }
 /*
 enter the number of rows:6
-     *
-    *A*
-   *A*A*
-  *A*A*A*
- *A*A*A*A*
-*A*A*A*A*A*
-
-
-*/
+    1
+   232
+  34543
+ 4567654
+567898765
+67891011109876
+ */
