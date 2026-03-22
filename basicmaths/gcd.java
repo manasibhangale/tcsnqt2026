5572aa6fb33774d69ee1cdf7cb0b073c3700b832
manasibhangale <manasibhangale2004@gmail.com>
2026-03-22 12:40:52 +0530

    linear search

--- basicmaths/gcd.java
+++ basicmaths/gcd.java
@@ -7,10 +7,10 @@ public class gcd {
         int n1=sc.nextInt();
         System.out.print("Enter the number 2:");
         int n2=sc.nextInt();
-        for(int i=Math.min(n1, n2);i>=1;i--){
+        for(int i=Math.min(n1, n2);i>=1;i--){ //👍
             if(n1%i==0 && n2%i==0){
                 System.out.print(i);
-                break;
+                break; //👍
             }
         }
     }
