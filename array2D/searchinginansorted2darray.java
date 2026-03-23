91c403d8a07e39ee2c895ce15391c2f9f859599f
manasibhangale <manasibhangale2004@gmail.com>
2026-03-23 18:03:04 +0530

    staircase sort on an sorted matrix which is sorted in row as well as colum

---
+++ array2D/searchinginansorted2darray.java
@@ -0,0 +1,28 @@
+package array2D;
+
+public class searchinginansorted2darray {
+    public static boolean staircasesort(int arr[][],int key){
+        int row=0,col=arr[0].length-1;
+        while(row<=arr.length && col>=0){
+            if(key==arr[row][col]){
+                System.out.println("the element is found at"+"("+row+","+col+")");
+                return true;
+            }else if(key<arr[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        int arr[][]={{10,20,30,40},    
+                     {15,25,35,45},
+                     {27,29,37,48},
+                     {32,33,39,50}
+        };
+        int key=39;
+        staircasesort(arr, key);
+    }
+}
+
