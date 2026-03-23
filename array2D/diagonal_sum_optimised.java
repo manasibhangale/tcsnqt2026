861b0cf45faef2eaa48723527f3cd6d3c6f46f13
manasibhangale <manasibhangale2004@gmail.com>
2026-03-23 17:17:59 +0530

    optimised diagonal sum with lesser TC

---
+++ array2D/diagonal_sum_optimised.java
@@ -0,0 +1,25 @@
+package array2D;
+
+public class diagonal_sum_optimised {
+    public static int diagonalsum(int arr[][]){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){//👍
+            sum+=arr[i][i];//both the i and j are same//👍
+            //condition 2: i+j=n-1
+            //n=arr.length
+            //then j=n-1-i
+            if(i!=arr.length-1-i)//👍
+            sum+=arr[i][arr.length-1-i];//👍
+        }
+        return sum;
+    }
+    public static void main(String args[]){
+        int arr[][]={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        System.out.println(diagonalsum(arr));
+    }
+}
