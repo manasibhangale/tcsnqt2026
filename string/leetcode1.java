ecec263947d8355908490dde2895abbfbd89ab75
manasibhangale <manasibhangale2004@gmail.com>
2026-03-24 15:54:16 +0530

    check for the brackets

---
+++ string/leetcode1.java
@@ -0,0 +1,55 @@
+package string;
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+  */
+public class leetcode1 {
+     public boolean isValid(String s) {
+        while(s.contains("()")||s.contains("{}")||s.contains("[]")){
+            s=s.replace("()","");
+            s=s.replace("{}","");
+            s=s.replace("[]","");
+        }
+        if(s.length()==0){
+            return true;
+        }
+        return false;
+    }
+}
