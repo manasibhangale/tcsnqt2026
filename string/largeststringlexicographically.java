edb7efc2111c66d5dc1142eb8ceb8a54da9e5a72
manasibhangale <manasibhangale2004@gmail.com>
2026-03-24 12:01:10 +0530

    find the largest string in an array lexicographically

---
+++ string/largeststringlexicographically.java
@@ -0,0 +1,16 @@
+package string;
+class largeststringlexicographically{
+    public static void largest(String arr[]){
+        String largest=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(largest.compareTo(arr[i])<0){
+                largest=arr[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    public static void main(String args[]){
+        String arr[]={"apple","pineapple","mango","starfruit","watermelon" };
+        largest(arr);
+    }
+}
