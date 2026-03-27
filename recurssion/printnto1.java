b4d3ec78b9bbc77ca0d0df5a1bcae9a9006a80cc
manasibhangale <manasibhangale2004@gmail.com>
2026-03-27 15:41:42 +0530

    print from n to 1 by using recursion(decreasing order)

---
+++ recurssion/printnto1.java
@@ -0,0 +1,15 @@
+package recurssion;
+
+public class printnto1 {
+    public static void printDec(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+    public static void main(String args[]){
+        printDec(5);
+    }
+}
