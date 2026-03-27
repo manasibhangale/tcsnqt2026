de7e1e3a49a9450a6904a3406259ca928e3bd21c
manasibhangale <manasibhangale2004@gmail.com>
2026-03-27 16:43:01 +0530

    find the last occurence of the key in the array and return its index

--- recurssion/firstoccurence.java
+++ recurssion/firstoccurence.java
@@ -2,13 +2,13 @@ package recurssion;
 
 public class firstoccurence {
     public static int firstocc(int arr[],int i,int key){
-        if(i==arr.length-1){
+        if(i==arr.length-1){//if the whole array is traversed and key is not found then the key doesnt exist so return -1.
             return -1;
         }
-        if(arr[i]==key){
+        if(arr[i]==key){//if the current index is equal to the key then return the current index
             return i;
         }
-        return firstocc(arr,i+1,key);
+        return firstocc(arr,i+1,key);//if the current index above is not the key then we check for the next index by i+1 recursively
     }
     public static void main(String args[]){
         int arr[]={1,2,3,4,6,7,5,7,6,7,8,9,1};
---
+++ recurssion/lastoccurence.java
@@ -0,0 +1,18 @@
+package recurssion;
+
+public class lastoccurence {
+    public static int lastocc(int arr[],int i,int key){
+        if(i==arr.length-1){
+            return -1;
+        }
+        int isfound=lastocc(arr,i+1,key);
+        if(isfound ==-1 && arr[i]==key){
+            return i;
+        }
+        return isfound;
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,6,4,5,7,6,9,8,6,9};
+        System.out.println(lastocc(arr,0,6));
+    }
+}
