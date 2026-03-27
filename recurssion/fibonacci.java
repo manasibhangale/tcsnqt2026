68351ad7b41d57fc39b3e52db6bda9b04f731a77
manasibhangale <manasibhangale2004@gmail.com>
2026-03-27 16:01:05 +0530

    fibonacci series using recursion

---
+++ recurssion/fibonacci.java
@@ -0,0 +1,13 @@
+package recurssion;
+
+public class fibonacci {
+    public static int fib(int n){
+        if(n==1||n==0){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String args[]){
+        System.out.println(fib(5));
+    }
+}
