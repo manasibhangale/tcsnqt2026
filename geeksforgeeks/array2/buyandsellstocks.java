6bcd37fb3bbeadfc660965222175cabc702740ba
manasibhangale <manasibhangale2004@gmail.com>
2026-04-16 12:22:25 +0530

    buy and sell stocks

---
+++ geeksforgeeks/array2/buyandsellstocks.java
@@ -0,0 +1,21 @@
+package geeksforgeeks.array2;
+
+public class buyandsellstocks {
+    public static int maxprofit(int arr[]){
+        int buyingprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<arr.length;i++){
+            if(buyingprice<arr[i]){
+                int profit=arr[i]-buyingprice;
+                maxprofit=Math.max(maxprofit,profit);
+            }else{
+                buyingprice=arr[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String args[]){
+        int arr[]={11,2,3,4,1,22,42};
+        System.out.println(maxprofit(arr));
+    }
+}
